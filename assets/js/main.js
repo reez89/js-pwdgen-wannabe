9eba7a27e76000c85a5207a31b3a1e908720da0c
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-12 13:48:16 +0100

    Finish
    Everything works fine.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+
+var name = prompt("Qual'è il tuo nome?");
+var surname = prompt("Qual'è il tuo cognome?");
+var prefColor = prompt("Qual'è il tuo lore preferito?");
+
+document.getElementById('password').innerHTML = name+surname+prefColor+19;
